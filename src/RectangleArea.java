25905f6ab4b5b34aa6b590b6f55ff5a27f07f9e3
unknown <naveenchandravanshi@gmail.com>
2015-04-20 16:38:06 +0530

    test

---
+++ src/RectangleArea.java
@@ -0,0 +1,17 @@
+/**
+ * Created by naveen on 4/20/2015.
+ */
+public class RectangleArea {
+    public static void main(String args []) {
+
+        System.out.println("Hello World");
+    }
+
+    private static Double calculateArea(Double length, Double breadth) {
+        Double area = 0.0;
+        if(length > 0.0 && breadth > 0.0)
+            area =  length*breadth;
+
+        return area;
+    }
+}
