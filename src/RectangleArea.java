31db428aac153ab713539b3008d6c998c2ac45db
unknown <naveenchandravanshi@gmail.com>
2015-04-21 09:44:32 +0530

    added overloading method to calculate the area of square

--- src/RectangleArea.java
+++ src/RectangleArea.java
@@ -4,13 +4,22 @@
 public class RectangleArea {
     public static void main(String args []) {
 
-        System.out.println("Hello World");
+        System.out.println(calculateArea(5.0, 8.0));
+        System.out.println(calculateArea(7.0));
     }
 
     private static Double calculateArea(Double length, Double breadth) {
         Double area = 0.0;
         if(length > 0.0 && breadth > 0.0)
             area =  length*breadth;
+
+        return area;
+    }
+
+    private static Double calculateArea(Double side) {
+        Double area = 0.0;
+        if(side > 0.0)
+            area =  side*side;
 
         return area;
     }
